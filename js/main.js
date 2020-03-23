2c6cfd931a7e5a04583d344da1c7a52e8dd01e90
tarq-p <pierluigi.tarquinio90@gmail.com>
2020-03-23 16:32:01 +0100

    ok poster e aggiunta per la serie tv, stelle non funzionanti

--- js/main.js
+++ js/main.js
@@ -1,14 +1,65 @@
+
 $(document).ready(function() {
 
     var source = $('#films-template').html();
     var template = Handlebars.compile(source);
 
+
+    var apiBaseUrl = 'https://api.themoviedb.org/3';
+    var imgBaseUrl = 'https://image.tmdb.org/t/p/';
+    var imgSize = 'w342';
+
     $('#btn-search').click(function() {
-        var input = $('.search').val();
-        console.log(input);
+        var valoreInput = $('input').val();
+        $('.container-film').html('');
+
 
         $.ajax({
-            url: 'https://api.themoviedb.org/3/search/tv?api_key=e99307154c6dfb0b4750f6603256716d&language=it_IT&query=scrubs',
+            url: 'https://api.themoviedb.org/3/search/movie',
+
+            data: {
+                api_key: 'cd0758b773006b78db6362a500a13095',
+                query: valoreInput,
+                language: 'it-IT'
+            },
+
+            method: 'GET',
+            success: function(data) {
+                console.log(data);
+                var films = data.results;
+                for (var i = 0; i < films.length; i++) {
+                    var film = films[i];
+                    var voto_effettivo = Math.ceil(films[i].vote_average / 2);
+                    console.log(film);
+                    var filmstemplate = {
+                        cover: poster(film.poster_path),
+                        titolo : film.title,
+                        titoloOriginale: film.original_name,
+                        lingua: film.original_language,
+                        voto: stars(voto_effettivo)
+                    };
+                    console.log(filmstemplate);
+
+                    var cardsFilm = template(filmstemplate);
+                    $('.container-film').append(cardsFilm);
+                }
+
+            },
+            error: function(error) {
+                alert('Non va');
+            }
+        });
+
+
+        $.ajax({
+            url:  'https://api.themoviedb.org/3/search/tv',
+
+            data: {
+                api_key: 'cd0758b773006b78db6362a500a13095',
+                query: valoreInput,
+                language: 'it-IT'
+            },
+
             method: 'GET',
             success: function(data) {
                 console.log(data);
@@ -17,21 +68,41 @@ $(document).ready(function() {
                     var film = films[i];
                     console.log(film);
                     var filmstemplate = {
-                        titolo: film.title,
+                        cover: poster(film.poster_path),
+                        titolo : film.title,
                         titoloOriginale: film.original_name,
                         lingua: film.original_language,
-                        voto: film.vote_average
+                        voto: film.vote_average                    
                     }
                     console.log(filmstemplate);
 
                     var cardsFilm = template(filmstemplate);
-                    $('.container1').append(cardsFilm);
+                    $('.container-film').append(cardsFilm);
                 }
 
             },
-            error: function(err) {
+            error: function(error) {
                 alert('Non va');
             }
         });
+
+
+        function stars(vote){
+            var star = '';
+            for (var k = 0; k < voto; k++) {
+              star += '<i class="fas fa-star"></i>';
+            }
+            return star;
+        }
+
+        function poster(path) {
+        if (path !== null) {
+            return imgBaseUrl + imgSize + path;
+        } else {
+            return 'https://image.freepik.com/vettori-gratuito/errore-404-non-trovato-glitch-effect_8024-4.jpg';
+
+        };
+    };
+
     });
 });
